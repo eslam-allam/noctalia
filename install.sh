b455791164361a7b2d880c861d739cb46d2f6922
Eslam Allam <eslamallam73@gmail.com>
2025-11-22 14:48:06 +0300

    refactor: Move installer logic to function and add rofi

--- install.sh
+++ install.sh
@@ -1,16 +1,36 @@
 #!/usr/bin/env bash
 
-hyprTarget="$HOME/.config/hypr"
-hyprBackup="$hyprTarget.$(date +%Y%m%d).bak"
+baseTarget="$HOME/.config"
 
-if [[ -e "$hyprTarget" ]]; then
-  if ! mv "$hyprTarget" "$hyprBackup"; then
-    echo "Failed to backup $hyprTarget to $hyprBackup"
-    exit 1
+function connect {
+  component="$1"
+  target="$baseTarget/$component"
+  backup="$target.$(date +%Y%m%d).bak"
+
+  link_source="$PWD/$component"
+
+  if [[ -e "$target" ]]; then
+    if [[ "$(readlink -f "$target")" = "$link_source" ]]; then
+      echo "Target $target is already linked to $link_source"
+      return 0
+    fi
+    if ! mv "$target" "$backup"; then
+      echo "Failed to backup $target to $backup"
+      return 1
+    fi
   fi
+
+  if ! ln -s "$link_source" "$target"; then
+    echo "Failed to link $link_source to $target"
+    return 1
+  fi
+
+}
+
+if ! connect hypr; then
+  exit 1
 fi
 
-if ! ln -s "$PWD/hypr" "$hyprTarget"; then
-  echo "Failed to link $PWD/hypr to $hyprTarget"
+if ! connect rofi; then
   exit 1
 fi
